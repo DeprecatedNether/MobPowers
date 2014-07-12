42542a24eb781a77e71527e1060dee581a478943
DeprecatedNether <deprecatednether@gmail.com>
2014-07-12 17:28:35 +0200

    Add reload command

--- src/main/java/com/tenjava/entries/DeprecatedNether/t2/commands/MobPowersCommandHandler.java
+++ src/main/java/com/tenjava/entries/DeprecatedNether/t2/commands/MobPowersCommandHandler.java
@@ -1,5 +1,6 @@
 package com.tenjava.entries.DeprecatedNether.t2.commands;
 
+import com.tenjava.entries.DeprecatedNether.t2.MobPowersMethods;
 import com.tenjava.entries.DeprecatedNether.t2.TenJava;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -46,6 +47,15 @@ public class MobPowersCommandHandler implements CommandExecutor {
             player.getInventory().addItem(wand);
             player.sendMessage(ChatColor.GREEN + "Right click with your wand to bring up the user interface.");
             return true;
+        }
+        if (args[0].equals("reload")) {
+            if (!sender.hasPermission("mobpowers.reload")) {
+                sender.sendMessage(ChatColor.RED + "No access.");
+                return true;
+            }
+            main.reloadConfig();
+            main.methods.particles.cancel();
+            main.methods = new MobPowersMethods(main); // This reloads the materials/enabled mobs from the config, as well as restarts the particle task if need be
         }
         player.sendMessage(ChatColor.RED + "Invalid argument '" + args[0] + "', try '/mobpowers' or '/mobpowers wand'");
         return true;
