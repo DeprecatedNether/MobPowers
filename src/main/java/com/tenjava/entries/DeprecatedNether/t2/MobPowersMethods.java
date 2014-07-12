7d9f62a64323252987a7d3768cbbfb2890f99406
DeprecatedNether <deprecatednether@gmail.com>
2014-07-12 11:49:23 +0200

    Implement give token method

--- src/main/java/com/tenjava/entries/DeprecatedNether/t2/MobPowersMethods.java
+++ src/main/java/com/tenjava/entries/DeprecatedNether/t2/MobPowersMethods.java
@@ -1,16 +1,56 @@
 package com.tenjava.entries.DeprecatedNether.t2;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class MobPowersMethods {
+
+    List<EntityType> mobs = new ArrayList<EntityType>();
+    private TenJava main;
+
+    public MobPowersMethods(TenJava main) {
+        this.main = main;
+        // load mobs we have superpowers for
+        mobs.add(EntityType.CREEPER);
+        mobs.add(EntityType.ENDERMAN);
+        mobs.add(EntityType.GHAST);
+        mobs.add(EntityType.SQUID);
+        mobs.add(EntityType.HORSE);
+        mobs.add(EntityType.SPIDER);
+    }
+
     /**
      * Gives the player a token for killing a mob based on the percentage set in the config.
      * For example, if the entity type's drop chance is set to 0, this will never do anything.
      * @param entity
      */
-    public void giveToken(Player player, EntityType entity) {
-
+    public boolean giveToken(Player player, EntityType entity) {
+        if (!mobs.contains(entity)) {
+            return false;
+        }
+        Random random = new Random();
+        int chance = main.getConfig().getInt("powers." + entity.toString().toLowerCase() + ".drop-chance");
+        if (random.nextInt(100) + 1 > chance) { // gives us a number between 1 and 100. If it's less than or equal to "chance", do something. Larger the chance, more likely this will happen
+            return false;
+        }
+        File file = new File(main.getDataFolder(), "players.yml");
+        FileConfiguration fileConfiguration = YamlConfiguration.loadConfiguration(file);
+        fileConfiguration.set(player.getName(), 1);
+        try {
+            fileConfiguration.save(file);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
     /**
