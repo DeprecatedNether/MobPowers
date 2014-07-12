5b3c2b831ddaf8c42a0a8c8881217ae3c0f435ed
DeprecatedNether <deprecatednether@gmail.com>
2014-07-12 12:38:40 +0200

    Remove unused imports

--- src/main/java/com/tenjava/entries/DeprecatedNether/t2/commands/MobPowersCommandHandler.java
+++ src/main/java/com/tenjava/entries/DeprecatedNether/t2/commands/MobPowersCommandHandler.java
@@ -9,7 +9,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.material.MaterialData;
 
 public class MobPowersCommandHandler implements CommandExecutor {
 
--- src/main/java/com/tenjava/entries/DeprecatedNether/t2/listeners/TJInventoryEvent.java
+++ src/main/java/com/tenjava/entries/DeprecatedNether/t2/listeners/TJInventoryEvent.java
@@ -7,7 +7,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.ItemStack;
 
 public class TJInventoryEvent implements Listener {
     private TenJava main;
