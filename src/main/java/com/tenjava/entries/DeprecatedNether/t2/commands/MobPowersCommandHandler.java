6c4e98e91f8dc614b8729555678f6c34600e4faa
DeprecatedNether <deprecatednether@gmail.com>
2014-07-12 12:16:55 +0200

    Add constructor for commands handler

--- src/main/java/com/tenjava/entries/DeprecatedNether/t2/commands/MobPowersCommandHandler.java
+++ src/main/java/com/tenjava/entries/DeprecatedNether/t2/commands/MobPowersCommandHandler.java
@@ -1,13 +1,20 @@
 package com.tenjava.entries.DeprecatedNether.t2.commands;
 
+import com.tenjava.entries.DeprecatedNether.t2.TenJava;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class MobPowersCommandHandler implements CommandExecutor {
+
+    private TenJava main;
+
+    public MobPowersCommandHandler(TenJava main) {
+        this.main = main;
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args) {
-
-        return false;
+        return true;
     }
 }
