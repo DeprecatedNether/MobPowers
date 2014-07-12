b006d4f8a0953b505707cfa5cc768ed7cb401a19
DeprecatedNether <deprecatednether@gmail.com>
2014-07-12 12:49:22 +0200

    Create file

--- src/main/java/com/tenjava/entries/DeprecatedNether/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/DeprecatedNether/t2/TenJava.java
@@ -6,12 +6,25 @@ import com.tenjava.entries.DeprecatedNether.t2.listeners.KillEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
+
 public class TenJava extends JavaPlugin {
 
     public MobPowersMethods methods;
 
     @Override
     public void onEnable() {
+        File file = new File(getDataFolder(), "players.yml");
+        if (!file.isFile()) {
+            try {
+                file.createNewFile();
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+                getLogger().severe("Couldn't access/create the players.yml file, meaning we have no way of accessing the players' token data. I'll just disable myself.");
+                getServer().getPluginManager().disablePlugin(this);
+            }
+        }
         PluginManager pm = getServer().getPluginManager();
         pm.registerEvents(new InteractEvent(this), this);
         pm.registerEvents(new KillEvent(this), this);
