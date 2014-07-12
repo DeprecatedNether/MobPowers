d289bfb5b1d3af2c0251ceb8444805ee3f7f9f37
DeprecatedNether <deprecatednether@gmail.com>
2014-07-12 12:17:47 +0200

    Add check if sender is player

--- src/main/java/com/tenjava/entries/DeprecatedNether/t2/commands/MobPowersCommandHandler.java
+++ src/main/java/com/tenjava/entries/DeprecatedNether/t2/commands/MobPowersCommandHandler.java
@@ -1,9 +1,11 @@
 package com.tenjava.entries.DeprecatedNether.t2.commands;
 
 import com.tenjava.entries.DeprecatedNether.t2.TenJava;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class MobPowersCommandHandler implements CommandExecutor {
 
@@ -15,6 +17,11 @@ public class MobPowersCommandHandler implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "If you aren't a player, how are you supposed to kill mobs to get tokens?");
+            return true;
+        }
+
         return true;
     }
 }
