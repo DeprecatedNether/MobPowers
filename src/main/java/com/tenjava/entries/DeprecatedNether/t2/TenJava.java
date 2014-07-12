1472e99843a756ed1ad641c1ac2ebad3843415e8
DeprecatedNether <deprecatednether@gmail.com>
2014-07-12 12:21:39 +0200

    Add commands

--- src/main/java/com/tenjava/entries/DeprecatedNether/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/DeprecatedNether/t2/TenJava.java
@@ -8,7 +8,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class TenJava extends JavaPlugin {
 
-    MobPowersMethods methods;
+    public MobPowersMethods methods;
 
     @Override
     public void onEnable() {
--- src/main/java/com/tenjava/entries/DeprecatedNether/t2/commands/MobPowersCommandHandler.java
+++ src/main/java/com/tenjava/entries/DeprecatedNether/t2/commands/MobPowersCommandHandler.java
@@ -2,10 +2,14 @@ package com.tenjava.entries.DeprecatedNether.t2.commands;
 
 import com.tenjava.entries.DeprecatedNether.t2.TenJava;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.material.MaterialData;
 
 public class MobPowersCommandHandler implements CommandExecutor {
 
@@ -21,7 +25,22 @@ public class MobPowersCommandHandler implements CommandExecutor {
             sender.sendMessage(ChatColor.RED + "If you aren't a player, how are you supposed to kill mobs to get tokens?");
             return true;
         }
-
+        Player player = (Player) sender;
+        if (args.length == 0) {
+            main.methods.openGUI(player);
+            return true;
+        }
+        if (args[0].equalsIgnoreCase("wand")) {
+            ItemStack wand = new ItemStack(Material.STICK, 1);
+            wand.setDurability((byte)2); // We'll use durability for identifying
+            ItemMeta meta = wand.getItemMeta();
+            meta.setDisplayName(ChatColor.GOLD + "MobPowers Wand");
+            wand.setItemMeta(meta);
+            player.getInventory().addItem(wand);
+            player.sendMessage(ChatColor.GREEN + "Right click with your wand to bring up the user interface.");
+            return true;
+        }
+        player.sendMessage(ChatColor.RED + "Invalid argument '" + args[0] + "', try '/mobpowers' or '/mobpowers wand'");
         return true;
     }
 }
