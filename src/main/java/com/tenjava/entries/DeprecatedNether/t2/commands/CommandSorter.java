3e04be69de05ebcf31c26af03829e3bdcb12083f
DeprecatedNether <deprecatednether@gmail.com>
2014-07-12 11:35:02 +0200

    Add command sorter class

---
+++ src/main/java/com/tenjava/entries/DeprecatedNether/t2/commands/CommandSorter.java
@@ -0,0 +1,13 @@
+package com.tenjava.entries.DeprecatedNether.t2.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandSorter implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args) {
+
+        return false;
+    }
+}
