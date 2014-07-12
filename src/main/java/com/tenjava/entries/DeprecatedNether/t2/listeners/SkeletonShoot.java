70e2b826283bea338829691e76e1525188ddf7fc
DeprecatedNether <deprecatednether@gmail.com>
2014-07-12 14:44:29 +0200

    Add bow shoot listener

---
+++ src/main/java/com/tenjava/entries/DeprecatedNether/t2/listeners/SkeletonShoot.java
@@ -0,0 +1,25 @@
+package com.tenjava.entries.DeprecatedNether.t2.listeners;
+
+import com.tenjava.entries.DeprecatedNether.t2.TenJava;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityShootBowEvent;
+
+public class SkeletonShoot implements Listener {
+    private TenJava main;
+
+    public SkeletonShoot(TenJava main) {
+        this.main = main;
+    }
+
+    @EventHandler
+    public void shoot(EntityShootBowEvent e) {
+        if (!(e.getEntity() instanceof Player)) {
+            return;
+        }
+        if (main.methods.hasSkeletonAttack((Player) e.getEntity())) {
+            e.getProjectile().setVelocity(e.getProjectile().getVelocity().multiply(3));
+        }
+    }
+}
