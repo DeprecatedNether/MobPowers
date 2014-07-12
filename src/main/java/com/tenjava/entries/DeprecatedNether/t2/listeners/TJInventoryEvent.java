acdc1976dec46483061962c863747c4c01dc929e
DeprecatedNether <deprecatednether@gmail.com>
2014-07-12 12:34:55 +0200

    Add null check

--- src/main/java/com/tenjava/entries/DeprecatedNether/t2/listeners/TJInventoryEvent.java
+++ src/main/java/com/tenjava/entries/DeprecatedNether/t2/listeners/TJInventoryEvent.java
@@ -2,6 +2,7 @@ package com.tenjava.entries.DeprecatedNether.t2.listeners;
 
 import com.tenjava.entries.DeprecatedNether.t2.TenJava;
 import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,6 +20,8 @@ public class TJInventoryEvent implements Listener {
     public void interact(InventoryClickEvent e) {
         if (!e.getInventory().getTitle().equals("MobPowers Selector")) return;
         if (e.getCurrentItem() == null || e.getCurrentItem().getType() == Material.AIR) return;
-        main.methods.useSuperPower((Player)e.getWhoClicked(), main.methods.materialToEntityType(e.getCurrentItem().getType()));
+        EntityType type = main.methods.materialToEntityType(e.getCurrentItem().getType());
+        if (type == null) return;
+        main.methods.useSuperPower((Player)e.getWhoClicked(), type);
     }
 }
