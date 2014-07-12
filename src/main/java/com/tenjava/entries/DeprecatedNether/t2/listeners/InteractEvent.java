209f04c02f3fcbb2db1d347c8767e98ae3e61636
DeprecatedNether <deprecatednether@gmail.com>
2014-07-12 12:38:08 +0200

    Listen to the interact event for using the wand

--- src/main/java/com/tenjava/entries/DeprecatedNether/t2/listeners/InteractEvent.java
+++ src/main/java/com/tenjava/entries/DeprecatedNether/t2/listeners/InteractEvent.java
@@ -1,12 +1,28 @@
 package com.tenjava.entries.DeprecatedNether.t2.listeners;
 
 import com.tenjava.entries.DeprecatedNether.t2.TenJava;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class InteractEvent implements Listener {
     private TenJava main;
 
     public InteractEvent(TenJava main) {
         this.main = main;
+    }
+
+    @EventHandler
+    public void interact(PlayerInteractEvent e) {
+        if (!(e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK)) {
+            return;
+        }
+        ItemStack inhand = e.getPlayer().getItemInHand();
+        if (inhand == null || inhand.getType() != Material.STICK) return;
+        if (inhand.getDurability() != 2) return;
+        main.methods.openGUI(e.getPlayer());
     }
 }
